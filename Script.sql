f8457d9cedd92ef5e8a8f72fcf7f654c6b1605e9
RodUman1985 <108888005+RodUman1985@users.noreply.github.com>
2022-12-22 21:22:17 +0300

    1

--- Script.sql
+++ Script.sql
@@ -8,3 +8,8 @@ select* from users; -- выборка всех столбцов
 select Name as username, Age as userage from users;
 -- выборка с фильтрацией
 select * from users where age>20 and id<>4 or age=id;-- строго равно - =(== аналог java) . строго  не равно - <> (!= анлог Java)
+-- order by - для сортировки данных при выборке
+select * from users  order by bday;
+-- групировка
+select * from users group by name having count(name) >2;
+
